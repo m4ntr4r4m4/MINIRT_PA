3e21d34243b29d708fcc6f5e7419cd5324de4a09
Mantra16 <kurshiva@gmail.com>
2024-01-22 11:08:20 +0100

    plane_func.c norm done

--- src/render_scene/plane_func.c
+++ src/render_scene/plane_func.c
@@ -1,44 +1,46 @@
 #include "ray.h"
 
-int	 plane_intersaction(t_vars *vars, t_ray ray, double *t, t_pl plane)
+int	plane_intersaction(t_vars *vars, t_ray ray, double *t, t_pl plane)
 {
-	double dot;
+	double	dot;
 
 	dot = vecdot(vecnormalize(plane.normal), ray.direction);
 	if (fabs(dot) < 1e-4)
-		return 0;
-	*t  = vecdot(plane.normal, vecscale(vecminus(plane.point, ray.origin), 1/dot));
+		return (0);
+	*t = vecdot(plane.normal, \
+		vecscale(vecminus(plane.point, ray.origin), 1 / dot));
 	if (*t < 0)
-		return 0;
-	return 1;
+		return (0);
+	return (1);
 }
-void calculate_diffuse_plane(t_vec p, t_vec n, t_vars *vars , t_pl plane)
+
+void	calculate_diffuse_plane(t_vec p, t_vec n, t_vars *vars, t_pl plane)
 {
-	t_vec lightDirection = vecnormalize(vecminus(vars->light.origin , p)); // Direction to the spotlight
+	t_vec	light_dir;
+	double	cos_angle;
 
-    double dotProduct = vecdot( n, lightDirection);
-    double cosAngle = vecdot(n, lightDirection);
-    if (cosAngle > 0 )
-    {
-	    if (!shadowed(vars, p, vars->light, (char *)&plane))
-	    {
-		vars->color.r += (plane.r * cosAngle) * vars->light.ratio;
-		vars->color.g += (plane.g * cosAngle) * vars->light.ratio;
-		vars->color.b += (plane.b * cosAngle) * vars->light.ratio;
-	    }
-	vars->color.r = ft_clamp(vars->color.r, 0, 255);
-	vars->color.g = ft_clamp(vars->color.g, 0, 255);
-	vars->color.b = ft_clamp(vars->color.b, 0, 255);
-    }
-
+	light_dir = vecnormalize(vecminus(vars->light.origin, p));
+	cos_angle = vecdot(n, light_dir);
+	if (cos_angle > 0)
+	{
+		if (!shadowed(vars, p, vars->light, (char *)&plane))
+		{
+			vars->color.r += (plane.r * cos_angle) * vars->light.ratio;
+			vars->color.g += (plane.g * cos_angle) * vars->light.ratio;
+			vars->color.b += (plane.b * cos_angle) * vars->light.ratio;
+		}
+		vars->color.r = ft_clamp(vars->color.r, 0, 255);
+		vars->color.g = ft_clamp(vars->color.g, 0, 255);
+		vars->color.b = ft_clamp(vars->color.b, 0, 255);
+	}
 }
 
 int	calculate_plane_color(t_ray ray, t_vars *vars, t_pl plane, double t_plane)
 {
+	t_vec	intersection;
+
 	calculate_ambient(vars);
-
-	t_vec intersection = vecplus(ray.origin , vecscale(ray.direction ,t_plane));
-
+	intersection = vecplus(ray.origin, vecscale(ray.direction, t_plane));
 	calculate_diffuse_plane(intersection, plane.normal, vars, plane);
-	return (create_trgb(0, vars->color.r,  vars->color.g, vars->color.b));
+	return (create_trgb(0, vars->color.r, vars->color.g, vars->color.b));
 }
